4c4fef620e5c653a9729cb5c4610c731b8832d48
twitchysloth <twitchysloth@proton.me>
2026-06-09 13:34:41 +0300

    Revert to es only

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     lib: {
       entry: resolve(import.meta.dirname, 'src/index.ts'),
       name: 'NJReactUI',
+      formats: ['es'],
       fileName: 'index',
     },
 
